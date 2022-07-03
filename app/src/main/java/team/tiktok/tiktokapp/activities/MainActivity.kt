0f416217aac25cbc165d4c2cb9a2d753f7a38e0e
rogerzuchu21022019 <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-07-03 14:33:29 +0700

    finished Home and FollowingFM

--- app/src/main/java/team/tiktok/tiktokapp/activities/MainActivity.kt
+++ app/src/main/java/team/tiktok/tiktokapp/activities/MainActivity.kt
@@ -1,20 +1,33 @@
 package team.tiktok.tiktokapp.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import com.squareup.picasso.Picasso
 import team.tiktok.tiktokapp.R
 import team.tiktok.tiktokapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding  : ActivityMainBinding
-    lateinit var navController: NavController
+    private val IMAGE_REQ = 1
+    private var imagePath: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/team/tiktok/tiktokapp/fragments/home/HomeFM.kt
+++ app/src/main/java/team/tiktok/tiktokapp/fragments/home/HomeFM.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
@@ -20,11 +21,13 @@ import team.tiktok.tiktokapp.R
 import team.tiktok.tiktokapp.adapter.VideoAdapter
 import team.tiktok.tiktokapp.data.Video
 import team.tiktok.tiktokapp.databinding.FragmentHomeBinding
+import team.tiktok.tiktokapp.databinding.ItemVideoBinding
 
 
 class HomeFM : Fragment() ,VideoAdapter.OnClickItemInRecyclerView{
    lateinit var binding:FragmentHomeBinding
     private lateinit var adapter :VideoAdapter
+    lateinit var mediaController: MediaController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -60,13 +63,17 @@ class HomeFM : Fragment() ,VideoAdapter.OnClickItemInRecyclerView{
         adapter.stopListening()
     }
 
-    override fun onItemClick(position: Int, view: View) {
+
+    override fun onItemClick(itemVideoBinding: ItemVideoBinding, view: View) {
         val id = view.id
         if (id==R.id.civUser){
             findNavController().navigate(R.id.action_homeFM_to_detailUserFM)
         }
         if (id==R.id.tvFollowing){
             findNavController().navigate(R.id.action_homeFM_to_followingFM)
+            itemVideoBinding.tvFollowing.setTextColor(ContextCompat.getColor(view.context,R.color.white))
+
+
         }
         if (id==R.id.ivComment){
             findNavController().navigate(R.id.action_homeFM_to_commentBottomSheetFM)
