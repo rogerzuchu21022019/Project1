2da380a58a3b77b336a639aed009fd4c26018a38
rogerzuchu21022019 <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-07-05 21:34:03 +0700

    update layout

--- app/src/main/java/team/tiktok/tiktokapp/fragments/profile/ProfileFM.kt
+++ app/src/main/java/team/tiktok/tiktokapp/fragments/profile/ProfileFM.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,8 +18,10 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
 import team.tiktok.tiktokapp.R
+import team.tiktok.tiktokapp.adapter.detail.DetailViewpagerAdapter
 import team.tiktok.tiktokapp.databinding.FragmentProfileBinding
 
 
@@ -25,12 +29,14 @@ class ProfileFM : Fragment() {
     private val IMAGE_REQ = 1
     private var imagePath: Uri? = null
     lateinit var binding: FragmentProfileBinding
+    lateinit var adapter:DetailViewpagerAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(layoutInflater)
         clickImage()
+        initViewPager()
         clickButton()
         return binding.root
     }
@@ -46,6 +52,37 @@ class ProfileFM : Fragment() {
                 findNavController().navigate(R.id.action_profileFM_to_profileBottomSheetFM)
             }
         }
+    }
+
+    private fun initTabLayout() {
+        TabLayoutMediator(
+            binding.tab,
+            binding.vpDetail
+        ) { tab, position ->
+            when (position){
+                0->{
+                    tab.setIcon(R.drawable.list)
+                }
+                1->{
+                    tab.setIcon(R.drawable.resource_private)
+
+                }
+                2->{
+                    tab.setIcon(R.drawable.heart)
+
+                }
+                3->{
+                    tab.setIcon(R.drawable.favourite)
+
+                }
+            }
+        }.attach()
+    }
+
+    private fun initViewPager() {
+        adapter = DetailViewpagerAdapter(this)
+        binding.vpDetail.adapter = adapter
+        initTabLayout()
     }
 
     private fun clickImage() {
@@ -81,36 +118,17 @@ class ProfileFM : Fragment() {
         someActivityResultLauncher.launch(intent)
     }
 
-    private fun requestPermissionCamera() {
-        if (ContextCompat.checkSelfPermission(
-                requireActivity(),
-                Manifest.permission.CAMERA
-            )
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            accessCamera()
 
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(), arrayOf(
-                    Manifest.permission.CAMERA
-                ), IMAGE_REQ
-            )
-        }
-    }
-    private fun accessCamera() {
-        val intent = Intent()
-//        intent.type = "image/*" // if you want to you can use pdf/gif/video
-        intent.action = android.provider.MediaStore.ACTION_IMAGE_CAPTURE
-        someActivityResultLauncher.launch(intent)
-    }
 
 
     private var someActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == AppCompatActivity.RESULT_OK) {
             val data = result.data
             imagePath = data!!.data
-            Picasso.get().load(imagePath).into(binding.civAvatar)
+            val handle = Handler(Looper.myLooper()!!)
+            handle.postDelayed({
+                Picasso.get().load(imagePath).into(binding.civAvatar)
+            },800)
         }
 
     }
